764058ca570916d1f5e605e5f60e173e0e014a66
Kausik <kausikrajan.csvl@gmail.com>
2023-03-26 16:02:12 +0530

    comp 8

--- PIXARIUM/PIXARIUM/settings.py
+++ PIXARIUM/PIXARIUM/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1mihcp!l9-xf5z65elhrku7n)lo2t-x89smlz2o@w&=z*y2w##
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wallpaper-test5.onrender.com']
+ALLOWED_HOSTS = ['localhost','wallpaper-test5.onrender.com','127.0.0.1']
 
 
 # Application definition
